00695e613d69d534cb80743f347047d47ed37f00
inno <innocentiahan@gmail.com>
2026-06-25 07:32:56 +0800

    chore: finished withdrawal model

--- temanziro/constants/MoneyDetails.ts
+++ temanziro/constants/MoneyDetails.ts
@@ -10,5 +10,13 @@ export const WITHDRAW_STATUS = {
     DITOLAK: "ditolak",
 } as const;
 
+export const BANK_NAME = {
+    BCA: "BCA",
+    BRI: "BRI",
+    MANDIRI: "Mandiri",
+    BNI: "BNI",
+} as const;
+
 export type TopUpStatus = typeof TOPUP_STATUS[keyof typeof TOPUP_STATUS];
 export type WithdrawStatus = typeof WITHDRAW_STATUS[keyof typeof WITHDRAW_STATUS];
+export type BankName = typeof BANK_NAME[keyof typeof BANK_NAME];
---
+++ temanziro/domain/models/WithdrawalModel.ts
@@ -0,0 +1,11 @@
+import { BankName, WithdrawStatus } from "@/constants/MoneyDetails";
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+
+export interface Withdrawal {
+    bank_name: BankName;
+    withdrawal_account_number: string;
+    withdrawal_amount: number;
+    withdrawal_status: WithdrawStatus;
+    withdrawal_decline_reason: string | null;
+    withdrawal_requested_at: FirebaseFirestoreTypes.Timestamp;
+}
