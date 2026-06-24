ddac7e103de939f4c8bb3ff1832a92ea2db852b2
inno <innocentiahan@gmail.com>
2026-06-25 07:20:31 +0800

    chore: finished companion contant

---
+++ temanziro/constants/CompanionRating.ts
@@ -0,0 +1,35 @@
+import bronzeBadge from "@/assets/tier/bronze.svg";
+import silverBadge from "@/assets/tier/silver.svg";
+import goldBadge from "@/assets/tier/gold.svg";
+import platinumBadge from "@/assets/tier/platinum.svg";
+import bronzeMascot from "@/assets/tier/ziro-bronze.svg";
+import silverMascot from "@/assets/tier/ziro-silver.svg";
+import goldMascot from "@/assets/tier/ziro-gold.svg";
+import platinumMascot from "@/assets/tier/ziro-platinum.svg";
+
+export type TierType = {
+    name: string;
+    value: string;
+    minPoints: number;
+    badgeImg: string;
+    mascotImg: string;
+}
+
+export const TIERS: Record<string, TierType> = {
+    bronze: { name: "Bronze", value: "bronze", minPoints: 25, badgeImg: bronzeBadge, mascotImg: bronzeMascot },
+    silver: { name: "Silver", value: "silver", minPoints: 100, badgeImg: silverBadge, mascotImg: silverMascot },
+    gold: { name: "Gold", value: "gold", minPoints: 500, badgeImg: goldBadge, mascotImg: goldMascot },
+    platinum: { name: "Platinum", value: "platinum", minPoints: 1000, badgeImg: platinumBadge, mascotImg: platinumMascot }
+} as const;
+
+type StarRating = 1 | 2 | 3 | 4 | 5;
+
+export const FEELING_OPTIONS: Record<StarRating, string[]> = {
+    1: ["Dissapointed", "Ignored", "Uncomfortable", "Unpleasant"],
+    2: ["Unhelpful", "Rushed", "Misunderstood", "Unfriendly"],
+    3: ["Average", "Met Expectations", "Neutral"],
+    4: ["Safe", "Calm", "Heard", "Respected", "Helpful"],
+    5: ["Safe", "Trusted", "Inspired", "Empowered", "Valued", "Respected", "Very Satisfied"],
+} as const;
+
+export const TIER_TYPE_VALUES = Object.values(TIERS).map((tier) => tier.value);
