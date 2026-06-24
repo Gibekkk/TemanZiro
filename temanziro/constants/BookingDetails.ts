91d816ca23a84e09b4d6057f7bc0fa9c80c11d2e
inno <innocentiahan@gmail.com>
2026-06-24 22:07:06 +0800

    feat: add assets for activity and create declarations.d.ts to make TS know the import type

---
+++ temanziro/constants/BookingDetails.ts
@@ -0,0 +1,15 @@
+import nongkrongImg from "@/assets/images/nongkrong.jpg";
+import olahragaImg from "@/assets/images/olahraga.jpg";
+import belajarImg from "@/assets/images/belajar.jpg";
+import jalanImg from "@/assets/images/jalan.jpg";
+import hiburanImg from "@/assets/images/hiburan.jpg";
+import kulinerImg from "@/assets/images/kuliner.jpg";
+
+export const ACTIVITY_TYPE = [
+  { id: "1", title: "Nongkrong", vibes: "Cafe & Chill", image: nongkrongImg },
+  { id: "2", title: "Olahraga", vibes: "Sports & Active", image: olahragaImg },
+  { id: "3", title: "Belajar", vibes: "Study & Work", image: belajarImg },
+  { id: "4", title: "Jalan-jalan", vibes: "Explore & Travel", image: jalanImg },
+  { id: "5", title: "Hiburan", vibes: "Film & Events", image: hiburanImg },
+  { id: "6", title: "Kuliner", vibes: "Food & Dining", image: kulinerImg },
+];
