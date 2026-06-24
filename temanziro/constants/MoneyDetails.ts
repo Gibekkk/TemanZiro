689471bb9639a8175ed4d789539de2faeb26d269
inno <innocentiahan@gmail.com>
2026-06-25 06:48:39 +0800

    fix: fix punctuation

--- temanziro/constants/MoneyDetails.ts
+++ temanziro/constants/MoneyDetails.ts
@@ -1,11 +1,11 @@
 export const TOPUP_STATUS = {
-    MENUNGGU = "menunggu",
-    BERHASIL = "berhasil",
-    DITOLAK = "ditolak",
+    MENUNGGU: "menunggu",
+    BERHASIL: "berhasil",
+    DITOLAK: "ditolak",
 }
 
 export const WITHDRAW_STATUS = {
-    MENUNGGU = "menunggu",
-    BERHASIL = "berhasil",
-    DITOLAK = "ditolak",
+    MENUNGGU: "menunggu",
+    BERHASIL: "berhasil",
+    DITOLAK: "ditolak",
 }
