836bb1fffd9af6d3b61699e71cb759261bff6e95
inno <innocentiahan@gmail.com>
2026-06-30 23:20:28 +0800

    chore: add gender icon

--- temanziro/constants/UserDetails.ts
+++ temanziro/constants/UserDetails.ts
@@ -1,8 +1,46 @@
+import IconMale from "@/assets/icon/maleoff.svg";
+import IconFemale from "@/assets/icon/femaleoff.svg";
+import IconSecret from "@/assets/icon/rahasiaoff.svg";
+
 export const GENDER = {
     PRIA: "pria",
     WANITA: "wanita",
     RAHASIA: "rahasia",
 } as const;
+
+export const GENDER_DETAILS = {
+    [GENDER.PRIA]: {
+        label: "Pria",
+        icon: IconMale,
+    },
+    [GENDER.WANITA]: {
+        label: "Wanita",
+        icon: IconFemale,
+    },
+    [GENDER.RAHASIA]: {
+        label: "Rahasia",
+        icon: IconSecret,
+    },
+} as const;
+
+
+export const GENDER_ICON = {
+    [GENDER.PRIA]: {
+        label: "Pria",
+        iconOn: require("@/assets/icon/maleon.svg"),
+        iconOff: require("@/assets/icon/maleoff.svg"),
+    },
+    [GENDER.WANITA]: {
+        label: "Wanita",
+        iconOn: require("@/assets/icon/femaleon.svg"),
+        iconOff: require("@/assets/icon/femaleoff.svg"),
+    },
+    [GENDER.RAHASIA]: {
+        label: "Rahasia",
+        iconOn: require("@/assets/icon/rahasiaon.svg"),
+        iconOff: require("@/assets/icon/rahasiaoff.svg"),
+    }
+};
 
 export const USER_ROLE = {
     ADMIN: "admin",
