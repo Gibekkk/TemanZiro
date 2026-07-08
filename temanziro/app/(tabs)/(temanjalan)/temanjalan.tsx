7ca9007883f1c78555e0a61a69a2ec7bc5b1f467
inno <innocentiahan@gmail.com>
2026-07-08 20:54:43 +0800

    implement temanjalan screen into routing

--- temanziro/app/(tabs)/(temanjalan)/temanjalan.tsx
+++ temanziro/app/(tabs)/(temanjalan)/temanjalan.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import TemanJalanScreen from "@/views/screens/Common/TemanJalan/TemanJalanScreen";
 
 export default function TemanJalanIndex() {
-  return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Teman Jalan Screen</Text>
-      </View>
-    </SafeAreaProvider>
-  );
+  return <TemanJalanScreen />;
 }
