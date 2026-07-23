6273dfab1c603385f0504d7a528bfb672da05219
inno <innocentiahan@gmail.com>
2026-07-23 14:12:17 +0800

    chore: update routing for user routing

--- temanziro/app/(tabs)/(dashboard)/dashboard.tsx
+++ temanziro/app/(tabs)/(dashboard)/dashboard.tsx
@@ -1,10 +1,5 @@
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import DashboardUser from "@/views/screens/User/DashboardUserScreen/DashboardUserScreen"; // Sesuaikan path-nya
 
 export default function DashboardIndex() {
-  return (
-    <SafeAreaProvider>
-      <DashboardUser />
-    </SafeAreaProvider>
-  );
+  return <DashboardUser />;
 }
--- temanziro/app/(tabs)/(profile)/profile.tsx
+++ temanziro/app/(tabs)/(profile)/profile.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function ProfileIndex() {
   return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile Screen</Text>
-      </View>
-    </SafeAreaProvider>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile Screen</Text>
+    </View>
   );
 }
