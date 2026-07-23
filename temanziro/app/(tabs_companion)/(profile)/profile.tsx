4604107be976187bc3ca17bc755893229a52ef94
inno <innocentiahan@gmail.com>
2026-07-23 13:25:10 +0800

    chore: update routing for companion profile

--- temanziro/app/(tabs_companion)/(profile)/profile.tsx
+++ temanziro/app/(tabs_companion)/(profile)/profile.tsx
@@ -1,11 +1,6 @@
 import ProfileCompanion from "@/views/screens/Companion/ProfileCompanion/ProfileCompanion";
 import React from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function ProfileIndex() {
-  return (
-    <SafeAreaProvider>
-      <ProfileCompanion />
-    </SafeAreaProvider>
-  );
+  return <ProfileCompanion />;
 }
