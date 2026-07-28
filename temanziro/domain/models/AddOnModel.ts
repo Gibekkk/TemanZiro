b99745299b9809ec692ea7712390dc579a08ab35
inno <innocentiahan@gmail.com>
2026-07-28 14:27:32 +0800

    chore: update addon model to be efficient

--- temanziro/domain/models/AddOnModel.ts
+++ temanziro/domain/models/AddOnModel.ts
@@ -1,45 +1,39 @@
 import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
+export interface DocumentationAddOnDetails {
+    url_portfolio: string;
+    is_accepted: boolean;
+    portfolio_rejection_message?: string;
+    created_at: FirebaseFirestoreTypes.Timestamp;
+    updated_at?: FirebaseFirestoreTypes.Timestamp | null;
+}
 
 export interface AddOnsModel {
     uid: string;
-    add_on: {
-        add_on_name: string;
-        add_on_type: string;
-    };
+    is_active_documentation: boolean;
+    is_active_transportation: boolean;
+    documentation?: DocumentationAddOnDetails | null;
+    vehicles?: CompanionVehicleModel[] | null;
+    created_at: FirebaseFirestoreTypes.Timestamp;
+    updated_at?: FirebaseFirestoreTypes.Timestamp | null;
+}
+
+export interface CompanionVehicleModel {
+    id: string;
+    type: string;
+    model: string;
+    license_plate: string;
+    is_verified: boolean;
     is_active: boolean;
-}
-
-export interface DocumentationAddOnsModel {
-    uid: string;
-    url_portfolio: string;
-    is_verified: boolean;
-    portfolio_rejection_message?: string;
-    created_at: FirebaseFirestoreTypes.Timestamp | null;
-    updated_at: FirebaseFirestoreTypes.Timestamp | null;
-}
-
-export interface TransportationAddOnsModel {
-    uid: string;
-    transportation_type: string;
-    transportation_model: string;
-    transportation_license_plate: string;
-    is_verified: boolean;
-    created_at: FirebaseFirestoreTypes.Timestamp | null;
-    updated_at: FirebaseFirestoreTypes.Timestamp | null;
-}
-
-export interface CompanionTransportationDocumentModel {
-    uid: string;
-    url_sim: string;
-    sim_status: string;
+    url_sim?: string;
+    sim_status?: string;
     sim_rejection_message?: string;
-    url_stnk: string;
-    stnk_status: string;
+    url_stnk?: string;
+    stnk_status?: string;
     stnk_rejection_message?: string;
-    url_photo_vehicle_front: string;
-    vehicle_front_status: string;
+    url_vehicle_front_photo?: string | null;
+    vehicle_front_photo_status?: string;
     vehicle_front_rejection_message?: string;
-    url_photo_vehicle_side: string;
-    vehicle_side_status: string;
-    vehicle_side_rejection_message?: string;
+    url_vehicle_side_photo?: string | null;
+    vehicle_side_photo_status?: string;
+    vehicle_side_photo_rejection_message?: string;
 }
