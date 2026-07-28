d05aea7675122ddf7782cc9cf2592094d19ec461
inno <innocentiahan@gmail.com>
2026-07-28 19:40:36 +0800

    chore: change submission into documentation and transportation

--- temanziro/app/(tabs_companion)/(profile)/addons-submission.tsx
+++
@@ -1,6 +0,0 @@
-import AddOnsSubmission from "@/views/screens/Companion/ProfileCompanion/AddOnsCompanion/AddOnsSubmission";
-import React from "react";
-
-export default function AddOnsSubmissionRoute() {
-  return <AddOnsSubmission />;
-}
