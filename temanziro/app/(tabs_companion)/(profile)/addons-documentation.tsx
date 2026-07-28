2eddfd4d58d8871bd9378ceac7ce90ec7bcb91cf
inno <innocentiahan@gmail.com>
2026-07-28 15:42:20 +0800

    feat: create routing for addon sheet for documentation and transportation

---
+++ temanziro/app/(tabs_companion)/(profile)/addons-documentation.tsx
@@ -0,0 +1,6 @@
+import AddOnDocumentation from "@/views/screens/Companion/ProfileCompanion/AddOnsCompanion/AddOnDocumentation";
+import React from "react";
+
+export default function AddOnDocumentationRoute() {
+  return <AddOnDocumentation />;
+}
---
+++ temanziro/app/(tabs_companion)/(profile)/addons-transportation.tsx
@@ -0,0 +1,6 @@
+import AddOnTransportation from "@/views/screens/Companion/ProfileCompanion/AddOnsCompanion/AddOnTransportation";
+import React from "react";
+
+export default function AddOnTransportationRoute() {
+  return <AddOnTransportation />;
+}
