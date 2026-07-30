50752bb787321e096eb15c27f5b34c011e67da8f
inno <innocentiahan@gmail.com>
2026-07-30 11:58:51 +0800

    chore: update index hooks

--- temanziro/controllers/hooks/index.ts
+++ temanziro/controllers/hooks/index.ts
@@ -2,3 +2,5 @@ export { useAuth } from "./useAuth";
 export { useUserProfile } from "./useUserProfile";
 export { usePresence } from "./usePresence";
 export { useCompanionDashboard } from "./Companion/useCompanionDashboard";
+export { useMapLocation } from "./useMapLocation";
+
